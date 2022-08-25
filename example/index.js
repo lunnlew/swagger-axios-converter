7458b62feea9f1e76d473e3d4f9faaedbd38af85
lunnlew <4288315+lunnlew@users.noreply.github.com>
2022-08-25 14:23:15 +0800

    修改案例

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 const { Extractor, Generator, FileWriter } = require('../src/index')
 
-Extractor.fetch('https://app.swaggerhub.com/apiproxy/registry/wangyf/Cable/1.0.0').then(res => {
+Extractor.measure('https://app.swaggerhub.com/apiproxy/registry/wangyf/Cable/1.0.0').then(res => {
     FileWriter.write(Generator.run(Extractor.parse(res)), {
         output: './dist'
     })
